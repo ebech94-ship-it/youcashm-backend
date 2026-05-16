ad4de92f703d0c003fdd6bf3d0e5b2f5e6efd29a
Ekoko Bechem <ekokobechem09@gmail.com>
2026-05-16 17:02:06 +0100

    fix engine startup timing

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const gameRoutes = require("./routes/gameRoutes");
 
 // 👇 IMPORTANT: import your engine here
-const { startRound } = require("./game/gameEngine"); 
+const { startRound } = require("./game/roundEngine"); 
 // example: "./engine/roundEngine" or "./src/game/gameEngine"
 
 const app = express();
